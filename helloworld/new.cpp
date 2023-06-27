31e140f7ff4d9c3a52b00b2805b1d035946081e4
andella <lanadelrayy@gmail.com>
2023-06-27 20:59:02 +0300

    упражнение на вывод

--- helloworld/new.cpp
+++ helloworld/new.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
-#include <string>
  
 int main() {
-    std::string name;  
-    std::cout << "What is your name?\n";
-    std::cin >> name;  
-    std::cout << "Hello , " << name << "!\n";
+    std::cout << "C++ is a general-purpose programming language with a bias towards systems programming that\n - is a better C\n - supports data abstraction\n - supports object-oriented programming\n - supports generic programming.\n";
+    return 0; 
 }
