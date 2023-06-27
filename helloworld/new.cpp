7410d14d55b4a5b385b931a45a07b79214f78326
andella <lanadelrayy@gmail.com>
2023-06-27 19:10:46 +0300

    vv

--- helloworld/new.cpp
+++ helloworld/new.cpp
@@ -5,5 +5,5 @@ int main() {
     std::string name;  
     std::cout << "What is your name?\n";
     std::cin >> name;  
-    std::cout << "Hello , " << name << "!\n";
+    std::cout << "Hello, " << name << "!\n";
 }
